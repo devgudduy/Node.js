e80dfedffe643f05bb7ed7d45a08afc66e49c583
Guddu Kumar <guddu12022003@gmail.com>
2025-04-08 12:56:23 +0530

    Add files via upload

---
+++ 13-HTML-Pages/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+// console.log("public path is: -->", publicPath);
+
+app.use(express.static(publicPath));
+
+app.listen(5000);
