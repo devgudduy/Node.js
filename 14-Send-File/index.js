353c6ae0fa6ad9b025ff2bcae5a119d8c5ef5b91
Guddu Kumar <guddu12022003@gmail.com>
2025-04-09 12:35:30 +0530

    Add files via upload

---
+++ 14-Send-File/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+// console.log("public path is: -->", publicPath);
+
+app.get("", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+
+app.get("/help", (req, res) => {
+  res.sendFile(`${publicPath}/help.html`);
+});
+
+// app.get("*", (req, res) => {
+//   res.sendFile(`${publicPath}/4O4.html`);
+// });
+
+// app.use(express.static(publicPath));
+
+app.listen(5000);
---
+++ 15-ejs/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
+
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "John Doe",
+    age: 30,
+    location: "New York",
+  };
+
+  res.render("profile", { user });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+
+app.get("/help", (req, res) => {
+  res.sendFile(`${publicPath}/help.html`);
+});
+
+app.listen(5000);
