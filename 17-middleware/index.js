8b9a178acf0b7de3a21c7bac3d360705c71cdf25
Guddu Kumar <guddu12022003@gmail.com>
2025-04-17 23:02:18 +0530

    Update index.js

--- 17-middleware/index.js
+++ 17-middleware/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const portNumber = 3000;
 const reqFilter = (req, res, next) => {
   if (!req.query.age) {
     res.send("Please provide your age!");
