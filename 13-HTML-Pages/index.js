7d203c916f6543d46c2ff74354b8950886896aa2
Guddu Kumar <guddu12022003@gmail.com>
2025-04-25 23:27:03 +0530

    Update index.js

--- 13-HTML-Pages/index.js
+++ 13-HTML-Pages/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 // console.log("public path is: -->", publicPath);
+
 app.use(express.static(publicPath));
 
 app.listen(5000);
