e546a45d37cb7d09c09fe3f0d7ac16038a5ae216
Guddu Kumar <guddu12022003@gmail.com>
2025-04-26 23:57:39 +0530

    Update index.js

--- 15-ejs/index.js
+++ 15-ejs/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 app.set("view engine", "ejs");
+
 app.get("", (req, res) => {
   res.sendFile(`${publicPath}/index.html`);
 });
