0d71493dcf10bfe741b64377bf00217be0666dc4
Guddu Kumar <guddu12022003@gmail.com>
2025-05-04 23:56:11 +0530

    Update index.js

--- 18-connect-node-to-mongodb/index.js
+++ 18-connect-node-to-mongodb/index.js
@@ -3,7 +3,6 @@ const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 const database = "e-commerce";
 
-
 async function getData() {
   let result = await client.connect();
   let db = result.db(database);
