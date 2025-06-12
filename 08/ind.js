05422b6036f0e60028c424625ff9aaadf0229790
Guddu Kumar <guddu12022003@gmail.com>
2025-06-12 23:58:20 +0530

    Update ind.js

--- 08/ind.js
+++ 08/ind.js
@@ -3,4 +3,6 @@ const path = require("path");
 const dirPath = path.join(__dirname, "crud");
 const filePath = `${dirPath}/new.txt`;
 
+
+
 fs.writeFileSync(filePath, "This is a new file created using Node.js!");
