f039e1d1d3997daffecec122e042b1be27f0bac9
Guddu Kumar <guddu12022003@gmail.com>
2025-06-13 23:55:30 +0530

    Update index.js

--- 14-Send-File/index.js
+++ 14-Send-File/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
 const publicPath = path.join(__dirname, "public");
 // console.log("public path is: -->", publicPath);
 
