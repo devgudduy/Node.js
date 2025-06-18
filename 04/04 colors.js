856f4d07a701ef75a58a254ce67bf61b5d741ef0
Guddu Kumar <guddu12022003@gmail.com>
2025-06-18 23:59:45 +0530

    Update 04 colors.js

--- 04/04 colors.js
+++ 04/04 colors.js
@@ -1,6 +1,7 @@
 const colors = require("colors");
 console.log("package.json".green);
 console.log("package.json".red);
+
 console.log("Guddu Kumar".blue);
 console.log("Learning Node.js".yellow);
 console.log("I am Guddu Kumar Learning Node.js".rainbow);
