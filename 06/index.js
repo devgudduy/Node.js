28216b1216eef5bfe1c67a37ac17cdc40fb2c408
Guddu Kumar <guddu12022003@gmail.com>
2025-06-27 00:00:11 +0530

    Update index.js

--- 06/index.js
+++ 06/index.js
@@ -1,7 +1,6 @@
 // console.log(process.argv[1]);
 const fs = require("fs");
 const input = process.argv;
-
 if (input[2] === "add") {
   fs.writeFileSync(input[3], input[4]);
 } else if (input[2] === "remove") {
